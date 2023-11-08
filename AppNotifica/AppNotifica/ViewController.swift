7f37813b69efba1d7005bfc1d5fdd63be73ab539
IFB-BIOTIC-16 <ifb-biotic-16@iMac-de-IFB-BIOTIC-16.local>
2023-11-08 10:24:14 -0300

    First Commit

---
+++ AppNotifica/AppNotifica/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppNotifica
+//
+//  Created by IFB-BIOTIC-16 on 08/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
